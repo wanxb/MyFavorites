cf52d08ac3e48ece2d31586d3c25e3660109fc2e
王寻兵 <wangxunbing@yaodaitong.com.cn>
2023-11-06 17:31:07 +0800

    优化代码

--- MyFavorites.Core/Models/DataBaseSettings.cs
+++ MyFavorites.Core/Models/DataBaseSettings.cs
@@ -12,4 +12,11 @@
 
         public string BooksCollectionName { get; set; } = null!;
     }
+    public enum DatabaseType
+    {
+        Default,
+        MySQL,
+        MongoDB,
+        File
+    }
 }
--- MyFavorites.Core/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFavorites.Core.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
